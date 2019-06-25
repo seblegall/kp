49fb63af0b50ca408ce785230577b0fd6b23e01b
Sébastien Le Gall <s.legall@meetic-corp.com>
2019-06-25 22:54:08 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,43 @@
+# kp - kopy
+
+Kopy is a very simple tool offering a very strong power : ability to copy a file
+or a directory from local file system to a running container file system (almost) natively.
+
+* No need to install anything else than Kopy
+* No need to run any daemon or daemonSet (Kubernetes)
+* No need to change and rebuild your container image
+* No need to restart your container
+* No need to add anything in your Dockerfile
+
+`kp` simply works like a regular `cp` but from local to a running container.
+The container may be run by Docker or by Kubernetes, inside a pod. Kopy support both.
+
+## Usage
+
+Example with a running Docker container :
+
+```bash
+kp -c {container_id} /path/to/my/file /tmp
+``` 
+
+* `container_id` is the container ID you can get by running `docker ps`
+* `/path/to/my/file` is your source file you want to copy. It can be a single file
+or a directory. With Kopy you don't even have to precise your want to copy a directory.
+* `/tmp` is the path where to copy the files in the container. 
+
+Example with Kubernetes :
+
+````bash
+kp -c {pod_name} -n {namespace} -c {container} /path/to/my/file /tmp 
+````
+
+* `pod_name` is the pod running your container
+* `namespace` is the namespace running the pod. Default is `default`
+* `container` is the specific container running in the pod.
+If not set, it will take the first one. 
+
+## Install
+
+````bash
+go install github.com/seblegall/kp
+````
---
+++ main.go
@@ -0,0 +1,303 @@
+package main
+
+import (
+	"archive/tar"
+	"flag"
+	"fmt"
+	"github.com/sirupsen/logrus"
+	"io"
+	"io/ioutil"
+	"os"
+	"os/exec"
+	"path/filepath"
+)
+
+var (
+	container string
+	pod string
+	namespace string
+	dest string
+	src string
+	verbosity string
+)
+
+
+func main() {
+
+	initConfig()
+
+	files, err := ListFiles(src, dest)
+	if err != nil {
+		logrus.Fatal(err.Error())
+	}
+
+	if err := CopyFiles(container,pod, namespace, files); err != nil {
+		logrus.Fatal(err)
+	}
+
+}
+
+
+//initConfig reads the flags value, setup the log level an pars cmd args
+func initConfig() {
+	flag.StringVar(&container, "c", "", "container ID" )
+	flag.StringVar(&pod, "p", "", "pod name" )
+	flag.StringVar(&namespace, "n", "default", "kubernetes namespace")
+	flag.StringVar(&verbosity, "v", logrus.InfoLevel.String(), "Log level (debug, info, warn, error, fatal, panic")
+	flag.Parse()
+
+	if err := setUpLogs(os.Stdout, verbosity); err != nil {
+		logrus.Fatal(err.Error())
+	}
+
+	if container == "" && pod == ""{
+		logrus.Fatal("Please, provide a container ID or a pod name")
+	}
+
+	src = filepath.Clean(flag.Arg(0))
+	dest = filepath.Clean(flag.Arg(1))
+
+	logrus.Debugf("Copying files from %s to %s", src, dest)
+
+}
+
+//setUpLogs set on writer as output for the logs and set up the level
+func setUpLogs(out io.Writer, level string) error {
+
+	logrus.SetOutput(out)
+	lvl, err := logrus.ParseLevel(level)
+	if err != nil {
+		return err
+	}
+	logrus.SetLevel(lvl)
+	logrus.SetFormatter(&logrus.TextFormatter{
+		FullTimestamp:   true,
+		TimestampFormat: "2006-01-02 15:04:05",
+	})
+	return nil
+}
+
+
+//listFiles returns a list of file to copy from source to the destination
+//defined as a map
+func ListFiles(src, dest string) (map[string]string, error) {
+
+	files := make(map[string]string)
+
+	fi, err := os.Lstat(src)
+	if err != nil {
+		return  nil, err
+	}
+
+	//If src is a file, then we return a map with only one element
+	if !fi.Mode().IsDir() {
+		files[src] = filepath.Join(dest, fi.Name())
+		logrus.Debug(src," -> ", filepath.Join(dest, fi.Name()))
+		return files, nil
+	}
+
+	//If src is a directory, we find all files iteratively
+	errDir := filepath.Walk(src,
+		func(path string, info os.FileInfo, err error) error {
+			if err != nil {
+				return err
+			}
+			relPath, err := filepath.Rel(src, path)
+			if err != nil {
+				return err
+
+			}
+
+			destPath := filepath.Join(dest, fi.Name(), relPath)
+			files[path] = destPath
+			logrus.Debug(path," -> ", destPath)
+			return nil
+		})
+
+	if err != nil {
+		return nil, errDir
+	}
+
+	return files, nil
+}
+
+//copyFiles copy files from their source to the destination, inside the container | pod
+func CopyFiles(container, pod, namespace string, files map[string]string) error {
+
+	cmd := computeCommand(container, pod, namespace)
+
+	reader, writer := io.Pipe()
+	cmd.Stdin = reader
+
+	go func() {
+		defer writer.Close()
+
+		if err := createMappedTar(writer, "/", files); err != nil {
+			logrus.Fatal("Error creating tar archive:", err)
+		}
+	}()
+
+	logrus.Debugf("Running command: %s", cmd.Args)
+	stdoutPipe, err := cmd.StdoutPipe()
+	if err != nil {
+		return err
+	}
+
+	stderrPipe, err := cmd.StderrPipe()
+	if err != nil {
+		return err
+	}
+
+	if err := cmd.Start(); err != nil {
+		return fmt.Errorf("starting command %v : %s", cmd, err.Error())
+	}
+
+	stdout, err := ioutil.ReadAll(stdoutPipe)
+	if err != nil {
+		return err
+	}
+
+	stderr, err := ioutil.ReadAll(stderrPipe)
+	if err != nil {
+		return err
+	}
+
+	err = cmd.Wait()
+	if err != nil {
+		return fmt.Errorf("Running %s: stdout %s, stderr: %s, err: %v", cmd.Args, stdout, stderr, err)
+	}
+
+	if len(stderr) > 0 {
+		logrus.Debugf("Command output: [%s], stderr: %s", stdout, stderr)
+	} else {
+		logrus.Debugf("Command output: [%s]", stdout)
+	}
+	return nil
+}
+
+//computeCommand returns the right command to execute depending on the context (Docker or Kubernetes)
+func computeCommand(container, pod, namespace string) *exec.Cmd {
+	if pod == "" {
+		return exec.Command("docker", "exec", "-i", container, "tar", "xmf", "-",
+			"-C", "/", "--no-same-owner")
+	}
+
+	if container != "" {
+		return exec.Command( "kubectl", "exec", pod, "--namespace", namespace, "-c", container, "-i",
+			"--", "tar", "xmf", "-", "-C", "/", "--no-same-owner")
+	}
+
+	return exec.Command( "kubectl", "exec", pod, "--namespace", namespace, "-i",
+		"--", "tar", "xmf", "-", "-C", "/", "--no-same-owner")
+}
+
+//createMappedTar creates a tar from the files and write it to the specified writer.
+func createMappedTar(w io.Writer, root string, files map[string]string) error {
+	tw := tar.NewWriter(w)
+	defer tw.Close()
+
+	for src, dst := range files {
+		if err := addFileToTar(root, src, dst, tw); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+//addFileToTar adds a file to the specified tar.
+func addFileToTar(root string, src string, dst string, tw *tar.Writer) error {
+	var (
+		absPath string
+		err     error
+	)
+
+	absRoot, err := filepath.Abs(root)
+	if err != nil {
+		return err
+	}
+
+	if filepath.IsAbs(src) {
+		absPath = src
+	} else {
+		absPath, err = filepath.Abs(src)
+		if err != nil {
+			return err
+		}
+	}
+
+	tarPath := dst
+	if tarPath == "" {
+		tarPath, err = filepath.Rel(absRoot, absPath)
+		if err != nil {
+			return err
+		}
+	}
+	tarPath = filepath.ToSlash(tarPath)
+
+	fi, err := os.Lstat(absPath)
+	if err != nil {
+		return err
+	}
+	switch mode := fi.Mode(); {
+	case mode.IsDir():
+		tarHeader, err := tar.FileInfoHeader(fi, tarPath)
+		if err != nil {
+			return err
+		}
+		tarHeader.Name = tarPath
+
+		if err := tw.WriteHeader(tarHeader); err != nil {
+			return err
+		}
+	case mode.IsRegular():
+		tarHeader, err := tar.FileInfoHeader(fi, tarPath)
+		if err != nil {
+			return err
+		}
+		tarHeader.Name = tarPath
+
+		if err := tw.WriteHeader(tarHeader); err != nil {
+			return err
+		}
+
+		f, err := os.Open(absPath)
+		if err != nil {
+			return err
+		}
+		defer f.Close()
+
+		if _, err := io.Copy(tw, f); err != nil {
+			return fmt.Errorf("writing real file %s : %s", absPath, err.Error())
+		}
+	case (mode & os.ModeSymlink) != 0:
+		target, err := os.Readlink(absPath)
+		if err != nil {
+			return err
+		}
+		if filepath.IsAbs(target) {
+			logrus.Warnf("Skipping %s. Only relative symlinks are supported.", absPath)
+			return nil
+		}
+
+		tarHeader, err := tar.FileInfoHeader(fi, target)
+		if err != nil {
+			return err
+		}
+		tarHeader.Name = tarPath
+		if err := tw.WriteHeader(tarHeader); err != nil {
+			return err
+		}
+	default:
+		logrus.Warnf("Adding possibly unsupported file %s of type %s.", absPath, mode)
+		// Try to add it anyway?
+		tarHeader, err := tar.FileInfoHeader(fi, "")
+		if err != nil {
+			return err
+		}
+		if err := tw.WriteHeader(tarHeader); err != nil {
+			return err
+		}
+	}
+	return nil
+}
